2d5696d787c4f6d5be56d3764e3e86bbdc4d852a
gcastill0 <gilberto@hashicorp.com>
2020-01-07 14:22:50 -0500

    Initial work

---
+++ main.tf
@@ -0,0 +1,16 @@
+# Configure the Azure Provider
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=1.38.0"
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "Test"
+  location = "Canada Central"
+}
+
+# Generate output for next level workload
+output "azure_resource_group_name" {
+  value = "${azurerm_resource_group.example.name}"
+}

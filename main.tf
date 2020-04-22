757ff199df438a75c23d8de3db49370ac444f4cf
gcastill0 <gilberto@hashicorp.com>
2020-04-22 10:28:35 -0400

    Creating Interrupt Environment

--- main.tf
+++ main.tf
@@ -5,12 +5,12 @@ provider "azurerm" {
 }
 
 # Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = "ARG-poc-test"
+resource "azurerm_resource_group" "interrupt" {
+  name     = "interrupt-land"
   location = "Canada Central"
 }
 
 # Generate output for next level workload
 output "azure_resource_group_name" {
-  value = "${azurerm_resource_group.example.name}"
+  value = "${azurerm_resource_group.interrupt.name}"
 }

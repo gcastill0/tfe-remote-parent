115f3b697fd3b54d2e4c7253714075edc62625c8
Gilberto Castillo <gilberto@hashicorp.com>
2021-06-09 09:09:05 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
-# Configure the Azure Provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
-  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
-  version = "=1.38.0"
+  features {}
 }
 
 # Create a resource group
